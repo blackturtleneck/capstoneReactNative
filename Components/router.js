bd11064d98d79ae5567a1fed4969548d7070813a
sarahfeldmann <sarahf95@gmail.com>
2018-05-22 13:55:09 -0700

    add icons to nav

--- Components/router.js
+++ Components/router.js
@@ -5,11 +5,31 @@ import { Icon } from "react-native-elements";
 import Login from "./Login";
 import Messenger from "./Messenger";
 
+// import message from "./img/message-icon.png";
+// import profile from "./img/profile-icon.png";
+
 export const Tabs = TabNavigator({
   Login: {
-    screen: Login
+    screen: Login,
+    navigationOptions: {
+      tabBarLabel: "SIGN IN",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon name="md-person" type="ionicon" size={35} color={tintColor} />
+      )
+    }
   },
   Messenger: {
-    screen: Messenger
+    screen: Messenger,
+    navigationOptions: {
+      tabBarLabel: "MESSAGE",
+      tabBarIcon: ({ tintColor }) => (
+        <Icon
+          name="message-square"
+          size={35}
+          color={tintColor}
+          type="feather"
+        />
+      )
+    }
   }
 });
