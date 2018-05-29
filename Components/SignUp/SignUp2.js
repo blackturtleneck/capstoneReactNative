e2ee4c126d7e4ccd0e08603628a99296f16575f4
sarahfeldmann <sarahf95@gmail.com>
2018-05-28 19:32:28 -0700

    handle default age

--- Components/SignUp/SignUp2.js
+++ Components/SignUp/SignUp2.js
@@ -116,21 +116,9 @@ export default class SignUp2 extends Component {
   }
 
   nextStep(e) {
-    // e.preventDefault();
-    if (
-      this.state.matchDistance === undefined ||
-      this.state.matchDistance === null
-    ) {
-      this.setState({ matchDistance: 10 });
+    if (!this.state.matchAgeMax) {
+      this.setState({ matchAgeMax: 26, matchAgeMin: 21 });
     }
-
-    if (
-      this.state.matchGender === undefined ||
-      this.state.matchGender === null
-    ) {
-      this.setState({ matchGender: "both" });
-    }
-
     let data = {
       matchGender: this.state.matchGender,
       matchAgeMin: this.state.matchAgeMin,
