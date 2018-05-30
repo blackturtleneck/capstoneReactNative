03b40aeb3437101c0384c927cea6c9d5e14698b5
sarahfeldmann <sarahf95@gmail.com>
2018-05-30 04:14:03 -0700

    start the thing over

--- Components/SignUp/SignUpController.js
+++ Components/SignUp/SignUpController.js
@@ -12,7 +12,7 @@ export default class SignUpController extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      step: 5,
+      step: 1,
       fieldValues: {
         name: this.props.user.displayName,
         gender: null, //this.props.user.gender,
