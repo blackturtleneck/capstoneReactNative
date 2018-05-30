8d70fb84fe9ac707767c985e3eb7116e70246dbd
Zhanna Voloshina <zhannavoloshina@D-10-18-255-16.dhcp4.washington.edu>
2018-05-29 18:12:23 -0700

    date request worksgit add .git add . frik ya

--- Components/Dates.js
+++ Components/Dates.js
@@ -7,19 +7,31 @@ import {
     Button,
     StyleSheet,
     TouchableOpacity,
-    Image
+    Image,
+    TouchableHighlight
   } from "react-native"; 
   import DateMap from './DateMap';
   import SelectMultiple from 'react-native-select-multiple';
   import { firestore } from "../FirestoreConfig";
 
-const sun = ['1800','1900','2000','2100','2200'] ;
-const mon = ['m6', 'm7', '   m8', '   m9', '   m10'] ;
-const tue = ['    t6', '   t7', '   t8', '   t9', '   t10'] ;
-const wed = ['    w6', '   w7', '   w8', '   w9', '   w10'] ;
-const thu = ['    th6','   th7','   th8','   th9','   th10'] ;
-const fri = ['    f6', '   f7', '   f8', '   f9', '   f10'] ;
-const sat = ['    st6','   st7','   st8','   st9','   st10'] ;
+const sun = ['1','2','3','4','5'] ;
+const mon = ['6', '7', '8', '9', '10'] ;
+const tue = ['11', '12', '13', '14', '15'] ;
+const wed = ['16', '17', '18', '19', '20'] ;
+const thu = ['21','22','23','24','25'] ;
+const fri = ['26', '27', '28', '29', '30'] ;
+const sat = ['31','32','33','34','35'] ;
+
+
+const renderLabel = (label, style) => {
+    return(
+        <View style={{flexDirection: 'row', alignItems: 'center'}}>
+        <View style={{marginLeft:10}}>
+            <Text style={styles.whiteText}> {label} </Text>
+        </View>
+        </View>
+    )
+}
 
 
 export default class Dates extends Component {
@@ -100,39 +112,172 @@ export default class Dates extends Component {
         this.setState({
             array : selectedFruits
         })
-
-        var times = ["1900", "2000"]
-        let startTime = [];
-        let dayAv = '';
-        let start = '';
-        var thingtoAdd = {};
-        var timeToAdd = {};
-        var finalArr = {};
-        var i=0;
-        for (var key in times) {
-                 start = times[key].value;
-                 end = String(parseInt(times[key].value) + 100);
- 
-                 startTime.dayAv = "wed";
-     
-                 timeToAdd.start = start;
-                 timeToAdd.end = end;
-     
-                 startTime.times = timeToAdd;
-        console.log("startTImefor", startTime)
-        }
-        this.setState({
-            array : startTime 
-        });
-    
- 
       }
       
     onSubmit(event){
+        let times = this.state.array;
+        console.log(this.state.array, "ARRAY TEST!")
         this.setState({
             submitted : true
         });
 
+
+        let arrayTimes = [];
+        for(let i = 0; i <= times.length; i++){
+            let key = times[i]
+
+            for (var j in key) {
+                arrayTimes.push(key[j]);
+            }
+        }
+        console.log("ONE ARRAY", arrayTimes)
+        arrayTimes = arrayTimes.filter(function(item, index,inputArray){
+                return inputArray.indexOf(item) == index;
+        });
+        
+        const obj = [];
+
+        for (let i=0; i < arrayTimes.length; i++){
+            let day = '';
+            let start = '';
+            let end = '';
+
+            if(arrayTimes[i] <= 5){
+                day = "sun";
+                if(arrayTimes[i] == 1){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 2){
+                    start = 1900                
+                }
+                if(arrayTimes[i] == 3){
+                    start = 2000                
+                }
+                if(arrayTimes[i] == 4){
+                    start = 2100                   
+                }
+                if(arrayTimes[i] == 5){
+                    start = 2200   
+                }
+                end = start + 100
+            } else if (arrayTimes[i] <= 10) {
+                day = "mon"
+                if(arrayTimes[i] == 6){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 7){
+                    start = 1900                
+                }
+                if(arrayTimes[i] == 8){
+                    start = 2000              
+                }
+                if(arrayTimes[i] == 9){
+                    start = 2100                  
+                }
+                if(arrayTimes[i] == 10){
+                    start = 2200   
+                }
+                end = start + 100
+
+            } else if (arrayTimes[i] <= 15) {
+                day = "tues"
+                if(arrayTimes[i] == 11){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 12){
+                    start = 1900                 
+                }
+                if(arrayTimes[i] == 13){
+                    start = 2000                
+                }
+                if(arrayTimes[i] == 14){
+                    start = 2100                  
+                }
+                if(arrayTimes[i] == 15){
+                    start = 2200  
+                }
+                end = start + 100
+            } else if (arrayTimes[i] <= 20){
+                day = "wed"
+                if(arrayTimes[i] == 16){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 17){
+                    start = 1900                 
+                }
+                if(arrayTimes[i] == 18){
+                    start = 2000                
+                }
+                if(arrayTimes[i] == 19){
+                    start = 2100                   
+                }
+                if(arrayTimes[i] == 20){
+                    start = 2200   
+                }
+                end = start + 100
+            } else if (arrayTimes[i] <= 25){
+                day = "thurs"
+                if(arrayTimes[i] == 21){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 22){
+                    start = 1900                 
+                }
+                if(arrayTimes[i] == 23){
+                    start = 2000               
+                }
+                if(arrayTimes[i] == 24){
+                    start = 2100                  
+                }
+                if(arrayTimes[i] == 25){
+                    start = 2200   
+                }
+                end = start + 100
+            } else if (arrayTimes[i] <= 30){
+                day = "fri"
+                if(arrayTimes[i] == 26){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 27){
+                    start = 1900                 
+                }
+                if(arrayTimes[i] == 28){
+                    start = 2000                
+                }
+                if(arrayTimes[i] == 29){
+                    start = 2100                   
+                }
+                if(arrayTimes[i] == 30){
+                    start = 2200   
+                }
+                end = start + 100
+            } else {
+                day = "sat"
+                if(arrayTimes[i] == 31){
+                    start = 1800
+                }
+                if(arrayTimes[i] == 32){
+                    start = 1900                
+                }
+                if(arrayTimes[i] == 33){
+                    start = 2000                
+                }
+                if(arrayTimes[i] == 34){
+                    start = 2100                   
+                }
+                if(arrayTimes[i] == 35){
+                    start = 2200   
+                }
+                end = start + 100
+            }
+            obj.push({
+                "day" : day,
+                "times" : [{"start" : start, "end" : end}]
+            })
+        }
+        console.log("object", obj)
+
+    const finalTimeArray = obj;
 
     const time = new Date();
     let month = time.getMonth();
@@ -196,7 +341,7 @@ export default class Dates extends Component {
       sent: true,
       response: false,
       confirm: false,
-      startTime: this.state.array
+      startTime: finalTimeArray
     };
     const getDate = {
         id: time,
@@ -204,7 +349,7 @@ export default class Dates extends Component {
         sent: false,
         response: false,
         confirm: false,
-        startTime : this.state.array
+        startTime : finalTimeArray
       };
     firestore
       .collection("users")
@@ -221,13 +366,13 @@ export default class Dates extends Component {
       .doc(this.state.userEmail)
       .collection("dates")
       .doc(timeStamp)
-      .set(getDate);  
+      .set(getDate);   
 
     }
 
   render() {
     return (
-        <ScrollView>
+        <ScrollView style={styles.container}>
 
         {this.state.dateNames && 
         <View>
@@ -235,50 +380,56 @@ export default class Dates extends Component {
         <Text style = {styles.meethalfway}> Let's Meet Halfway! </Text>
         <TouchableOpacity style = {styles.button} onPress = {this.renderMoreInformation1} >
         <View style = {styles.buttonText}>
-                 <Text>Fremont Brewery</Text>
+                 <Text></Text>
         </View>
              <Image style={styles.image} source={require('./img/fremont.png')}/>
                 </TouchableOpacity>
 
                 <View>
-                {this.state.printText1 && <View> <Text> This kid- and dog-friendly beer hall with bench tables serves up seasonal ales and free pretzels.</Text> <TouchableOpacity  onPress = {this.renderMoreInformation1}> <Text> Select This Spot </Text> </TouchableOpacity> </View>}
+                {this.state.printText1 && <View> <Text> This kid- and dog-friendly beer hall with bench tables serves up seasonal ales and free pretzels.</Text> <TouchableOpacity style = {styles.nextbutton}  onPress = {this.renderMoreInformation1}> <Text style = {styles.whiteText}> SELECT THIS SPOT </Text> </TouchableOpacity> </View>}
                 </View>   
 
                 <TouchableOpacity style = {styles.button} onPress = {this.renderMoreInformation2}  >
                 <Image style={styles.image} source={require('./img/schilling.png')}/>
-                    <Text style = {styles.buttonText}>Schilling Cider</Text>
+                    <Text style = {styles.buttonText}></Text>
                 </TouchableOpacity> 
 
                 <View> 
-                {this.state.printText2 && <View> <Text> A great tasting bar with 30+ craft ciders, on tap and in bottles (food can be ordered for delivery).</Text> <TouchableOpacity onPress = {this.renderMoreInformation2}> <Text> Select This Spot </Text> </TouchableOpacity> </View>}
+                {this.state.printText2 && <View> <Text> A great tasting bar with 30+ craft ciders, on tap and in bottles (food can be ordered for delivery).</Text> <TouchableOpacity style = {styles.nextbutton}  onPress = {this.renderMoreInformation2}> <Text style = {styles.whiteText}> SELECT THIS SPOT </Text> </TouchableOpacity> </View>}
                 </View>   
 
                 <TouchableOpacity style = {styles.button} onPress = {this.renderMoreInformation3} >
                 <Image style={styles.image} source={require('./img/backdoor.png')}/>
-                    <Text>The Backdoor</Text>
+                    <Text></Text>
                 </TouchableOpacity>
 
                 <View> 
-                {this.state.printText3 && <View> <Text> Speakeasy-style lounge behind Roxy's Diner, for cocktails, small plates and occasional live music.</Text> <TouchableOpacity onPress = {this.renderMoreInformation3}> <Text> Select This Spot </Text> </TouchableOpacity> </View>}
+                {this.state.printText3 && <View> <Text> Speakeasy-style lounge behind Roxy's Diner, for cocktails, small plates and occasional live music.</Text> <TouchableOpacity style = {styles.nextbutton}  onPress = {this.renderMoreInformation3}> <Text style = {styles.whiteText} > SELECT THIS SPOT </Text> </TouchableOpacity> </View>}
                 </View>   
 
                 <TouchableOpacity style = {styles.button} onPress = {this.renderMoreInformation4} >
                 <Image style={styles.image} source={require('./img/barrelthief.png')}/>
-                    <Text>The Barrel Thief</Text>
+                    <Text></Text>
                 </TouchableOpacity>   
 
                <View>               
-                {this.state.printText4 && <View> <Text>Inventive cocktails and small plates in a warm, eco-friendly setting with regular tastings and classes.</Text> <TouchableOpacity onPress = {this.renderMoreInformation4}> <Text> Select This Spot </Text> </TouchableOpacity> </View>}                                
+                {this.state.printText4 && <View> <Text>Inventive cocktails and small plates in a warm, eco-friendly setting with regular tastings and classes.</Text> <TouchableOpacity style = {styles.nextbutton}  onPress = {this.renderMoreInformation4}> <Text style = {styles.whiteText}> SELECT THIS SPOT </Text> </TouchableOpacity> </View>}                                
                 </View>   
 
-                <TouchableOpacity style = {styles.button} onPress = {this._renderMap} >
-                    <Text>View on Map</Text>
-                </TouchableOpacity>     
-            
+                <TouchableOpacity style = {styles.mapbutton} onPress = {this._renderMap} >
+                    <Text style={styles.purpleText}>VIEW ON MAP</Text>
+                </TouchableOpacity>  
 
-                <TouchableOpacity style = {styles.button} onPress = {this.nextAvailability}>
-                    <Text>Next</Text>
-                </TouchableOpacity>   
+                
+                <View> 
+                {this.state.showMap && <View>  <Image source={require('./img/map.png')}/> </View>}
+                </View>      
+            
+            <View styles={styles.container}>
+                <TouchableHighlight style = {styles.nextbutton} onPress = {this.nextAvailability}>
+                    <Text style={styles.nextText}>NEXT</Text>
+                </TouchableHighlight>   
+             </View>
 
             </View>  
         }  
@@ -287,30 +438,36 @@ export default class Dates extends Component {
 
         <View>
 
-            <Text style = {styles.meethalfway}> Pick some days and times that work for you! </Text>
-        <View style={{flexDirection : 'row'}}>
+            <Text style = {styles.daysTime}> Pick some days and times that work for you! </Text>
+    
 
-            <Text> {"\n"} 6PM {"\n"}  {"\n"}  {"\n"}  7PM{"\n"}  {"\n"}  {"\n"}8PM {"\n"}  {"\n"}  {"\n"}
-             9PM {"\n"}  {"\n"}  {"\n"} 10PM {"\n"}</Text>
+            <Text style={styles.purpleText}>S    M   T    W     TH   F    SA</Text>
+        <View style={{flexDirection : 'row'}}>
+        <Text style={styles.purpleText}> {"\n"} 6PM{"\n"} 7PM{"\n"}{"\n"}8PM{"\n"}{"\n"}
+            9PM{"\n"}  {"\n"}  {"\n"} 10PM{"\n"}</Text>
 
             <SelectMultiple style = {styles.scheduler}
                     items={sun}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
+
 
             <SelectMultiple style = {styles.scheduler}
                     items={mon}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
-
+ 
             <SelectMultiple style = {styles.scheduler}
                     items={tue}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
 
 
@@ -318,36 +475,40 @@ export default class Dates extends Component {
                     items={wed}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
 
             <SelectMultiple style = {styles.scheduler} 
                     items={thu}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
 
             <SelectMultiple style = {styles.scheduler}
                     items={fri}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
 
             <SelectMultiple style = {styles.schedulerSat}
                     items={sat}
                     rowStyle={styles.checkbox}
                     selectedItems={this.state.selectedFruits}
-                    onSelectionsChange={this.onSelectionsChange} />
+                    onSelectionsChange={this.onSelectionsChange}
+                    renderLabel={renderLabel} />
 
 
 
         </View>
 
 
-                <TouchableOpacity style = {styles.button} onPress = {this.onSubmit} >
-                    <Text>Send Date Request</Text>
+                <TouchableOpacity style = {styles.nextbutton} onPress = {this.onSubmit} >
+                    <Text style={styles.whiteText}>Send Date Request</Text>
                 </TouchableOpacity>   
 
         </View>
@@ -361,7 +522,14 @@ export default class Dates extends Component {
 
 const styles = StyleSheet.create({
     meethalfway: {
-        fontSize : 30
+        fontSize : 30,
+        fontFamily: "Avenir-Light",
+        backgroundColor: "#ffffff",
+        textAlign: "center"
+    },
+    center : {
+        alignItems: "center",
+        textAlign: "center"
     },
     buttonText :{
         fontSize: 18,
@@ -393,13 +561,49 @@ const styles = StyleSheet.create({
     button: {
         alignItems: 'center',
         backgroundColor: "#DDDDDD",
+        marginTop: 5,
         height: 60,
-        marginTop: 5
+        fontFamily: "Avenir-Light"
       },
       mapbutton: {
-
+        backgroundColor: '#ffffff',
+        justifyContent : 'center',
+        marginLeft: 250,
+        marginTop: 10,
+        width: 100,
+        marginBottom: 10
       },
       nextbutton: {
-
+        backgroundColor: '#9ba2ff',
+        alignItems : 'center',
+        padding: 10,
+        width: 150,
+        marginLeft: 115,
+        borderRadius: 10,
+        marginTop: 10
+      },
+      container: {
+        backgroundColor: "#ffffff"
+      },
+      purpleText : {
+          color: "#9ba2ff"
+      },
+      whiteText : {
+        color: "#ffffff",
+        fontSize: 12,
+        fontFamily: "Avenir-Light"
+      },
+      nextText : {
+        color: "#ffffff",
+        fontSize: 18,
+        fontFamily: "Avenir-Light"
+      },
+      daysTime : {
+        fontSize : 25,
+        fontFamily: "Avenir-Light",
+        backgroundColor: "#ffffff",
+        textAlign: "center",
+        marginLeft: 10,
+        marginRight: 10
       }
 });
